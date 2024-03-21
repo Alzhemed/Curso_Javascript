c7155bba110b6fa292a760624dfe4663d4906277
Alzhemed <66705985+Alzhemed@users.noreply.github.com>
2024-03-21 09:20:26 -0300

    Add files via upload

---
+++ Estruturas de controle/for1.js
@@ -0,0 +1,21 @@
+let contador = 1;
+
+while (contador <= 10) {
+  console.log(`Contador = ${contador}`)
+  contador++;
+}
+
+console.log("");
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`Contador = ${i}`);
+}
+
+console.log("");
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7];
+
+for (let i = 0; i < notas.length; i++) {
+  console.log(`Nota ${i+1} = ${notas[i]}`);
+}
+
